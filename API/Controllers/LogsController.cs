7a6bb271d9f98eb4a7eb33e2e8d47ca3841239d8
mali <moohamedali571@gmail.com>
2025-03-05 15:23:22 +0200

    Add LogController and create GetBlockedAttempts endpoint

---
+++ API/Controllers/LogsController.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.DTOs;
+using Infrastructure.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogsController(ILogRepository logRepository) : ControllerBase
+    {
+        [HttpGet("blocked-attempts")]
+        public IActionResult GetBlockedAttempts([FromQuery] PaginationDTO? paginationDTO)
+        {
+            var blockedAttempts = logRepository.GetAllLogs(paginationDTO);
+            return Ok(blockedAttempts);
+        }
+    }
+}
